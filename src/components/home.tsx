1d99aff306310449c2f2f5a0f9df8a8af7cc1e0f
William Tyrrell <wftyrrell64@gmail.com>
2025-02-06 19:02:38 -0500

    edited home page

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -6,15 +6,26 @@ import Link from 'next/link';
 
 const Home = () => {
   return (
-    <div className="home-container w-full mb-8">
-      <header className="hero w-full  flex flex-col justify-center items-center bg-[#121212]">
-        <p className="text-white text-8xl mb-8 whitespace-nowrap"> Hello! I'm William Tyrrell</p>
+    <div className="home-container w-full mb-8 flex flex-col items-center">
+      <header className="hero flex flex-col justify-center items-center bg-[#121212] border-4 border-yellow-500 p-8 w-full w-[55vw] mb-4">
+      <div className="flex items-center justify-center">
+        <p className="text-yellow-500 text-8xl whitespace-nowrap">Hello!</p>
+        <p className="text-white text-8xl ml-4 whitespace-nowrap">I'm William Tyrrell</p>
+      </div>
       </header>
 
       <section className="intro w-full">
-        <h2 className="text-2xl font-bold text-white mb-2">About Me:</h2>
-        <p className="text-white text-lg">
-        Passionate about the intersection of hardware and software, I am an Electrical and Computer Engineering BS student also pursuing a Master's in Computer Science. My academic journey blends a deep understanding of electronics with advanced software development, enabling me to explore innovative solutions that bridge the gap between the physical and digital worlds. I thrive on tackling complex challenges and building systems that integrate both hardware and software seamlessly        </p>
+        <h2 className="text-4xl font-bold text-yellow-500 mb-2">About Me:</h2>
+        <p className="text-white font-bold text-xl mb-2">
+          As a skilled engineer, I am passionate about exploring opportunities to develop innovative hardware and software solutions in a new college graduate position.
+        </p>
+        <p className="text-white text-lg mb-2">
+          I have expertise in software development, including embedded systems, hardware description languages (HDL), full-stack development, and AI. Additionally, 
+          my hands-on experience with sensors, circuits, systems, and microcontrollers has strengthened my hardware and lab skills.
+        </p>
+        <p className="text-white text-lg"> 
+          Explore my projects, resume, and work experience to see how my skills can contribute to your team’s success.
+        </p>
       </section>
 
       <section className="projects">
